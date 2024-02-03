de32b99b4e05bb76e99afce114646817704fc8cb
unknown <way2codex@gmail.com>
2024-02-03 23:45:09 +0530

    api authentication added

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,17 +9,51 @@ use App\Models\ProductMaster;
 use Carbon\Carbon;
 use PDF;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 class ApiController extends Controller
 {
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+        return response()->json($response, $code);
+    }
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+            $success['name'] =  $user->name;
+            return $this->sendResponse($success, 'User login successfully.');
+        } else {
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
+        }
+    }
     public function customer_list()
     {
-        $customers = CustomerMaster::orderBy('id','desc')->get();
+        $customers = CustomerMaster::orderBy('id', 'desc')->get();
         return response()->json($customers);
     }
     public function product_list()
     {
-        $products = ProductMaster::orderBy('id','asc')->get();
+        $products = ProductMaster::orderBy('id', 'asc')->get();
         return response()->json($products);
     }
     public function store_customer(Request $request)
@@ -53,7 +87,7 @@ class ApiController extends Controller
         $data = EntryMaster::where('customer_id', $request->customer_id)
             ->whereBetween('entry_time', [$request->from_date . ' 00:00:00', $request->to_date . ' 23:59:59'])
             ->with('customer')
-            ->orderBy('entry_time','asc')
+            ->orderBy('entry_time', 'asc')
             ->get()
             ->transform(function ($item) {
                 $item->entry_time = \Carbon\Carbon::parse($item->entry_time);
@@ -62,7 +96,7 @@ class ApiController extends Controller
         if (count($data) > 0) {
             $fromDate = Carbon::parse($request->from_date)->format('d-m-Y');
             $toDate = Carbon::parse($request->to_date)->format('d-m-Y');
-            $customerName = $data->first()->customer->name; 
+            $customerName = $data->first()->customer->name;
             $productData = ProductMaster::get();
             $pdf = PDF::loadView(
                 'pdf.template',
@@ -74,7 +108,7 @@ class ApiController extends Controller
                     'toDate' => $toDate
                 ]
             );
-            $pdfPath = 'pdf/' . Str::slug($customerName, "_").'_pdf_'.uniqid() . '.pdf';
+            $pdfPath = 'pdf/' . Str::slug($customerName, "_") . '_pdf_' . uniqid() . '.pdf';
             $pdf->save(storage_path('app/public/' . $pdfPath));
             return response()->json(['pdf_url' => asset('storage/' . $pdfPath), 'status' => 'true']);
         } else {
--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/customer_list', [ApiController::class, 'customer_list']);
-Route::post('/store_customer', [ApiController::class, 'store_customer']);
-Route::post('/store_entry', [ApiController::class, 'store_entry']);
-Route::post('/entry_report', [ApiController::class, 'entry_report']);
-Route::get('/product_list', [ApiController::class, 'product_list']);
+Route::controller(ApiController::class)->group(function()
+{
+    Route::post('login', 'login');
+});
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/customer_list', [ApiController::class, 'customer_list']);
+    Route::post('/store_customer', [ApiController::class, 'store_customer']);
+    Route::post('/store_entry', [ApiController::class, 'store_entry']);
+    Route::post('/entry_report', [ApiController::class, 'entry_report']);
+    Route::get('/product_list', [ApiController::class, 'product_list']);
+});
